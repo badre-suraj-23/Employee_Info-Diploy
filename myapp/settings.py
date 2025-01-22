f6bdca4f3a1e59b01a42eb0c12dee46751a22e35
suraj badre <badresuraj@gmail.com>
2025-01-23 04:41:20 +0530

    Fixed whitenoise issue

--- myapp/settings.py
+++ myapp/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myapp.urls'
